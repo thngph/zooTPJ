56aa785789f50bb90a46cb127d26084d3c7ceed9
thngph <19520981@gm.uit.edu.vn>
2021-11-03 08:23:34 +0700

    alter some fields in model

--- animals/models.py
+++ animals/models.py
@@ -7,4 +7,5 @@ class Animal(models.Model):
     animal_ID = models.AutoField(unique=True, primary_key=True)
     animal_name = models.CharField(max_length=50, blank=False)
     description = models.CharField(max_length=1000)
-    img = models.CharField(max_length=1000)
+    img = models.FileField(default="")
+    profile_img = models.FileField(default="")
