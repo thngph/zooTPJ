d36635b0a6bf19c34771790c0dbcf4931bd962e3
UIT19520097 <67829285+UIT19520097@users.noreply.github.com>
2021-11-07 20:28:08 +0700

    thêm modal trong trang homepage

--- static/js/homepage-js/main.js
+++ static/js/homepage-js/main.js
@@ -1,200 +1,192 @@
-var counter=2;
-setInterval(function() {
-    document.getElementById('radio' +counter).checked = true;
-    counter++;
-    if(counter>3)
-    {
-        counter =1
-    } 
-},10000);
+var counter = 2;
+setInterval(function () {
+  document.getElementById("radio" + counter).checked = true;
+  counter++;
+  if (counter > 3) {
+    counter = 1;
+  }
+}, 10000);
 
-
-const $=document.querySelector.bind(document);
-const $$=document.querySelectorAll.bind(document);
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
 // XỬ LÍ VIDEO
-const swiperWrapperVideo=$('.video-animal .swiper-wrapper');
-const videoDisplay=$('.video-animal .video__category-place-display')
-const videos={
-    currentIndex:0,
-    videoList: [
-      {
-        name:'Khỉ',
-        link:'https://www.youtube.com/embed/LHpzjBydyGA'
-      },
-      {
-        name:'Ngựa vằn',
-        link:'https://www.youtube.com/embed/pI4KUIGrj9Y'
-      },
-      {
-        name:'Công',
-        link:'https://www.youtube.com/embed/fCx7VMnU6VY'
-      },
-      {
-        name:'Gấu',
-        link:'https://www.youtube.com/embed/q0BYoCNDK8w'
-      },
-      {
-        name:'Hươu cao cổ',
-        link:'https://www.youtube.com/embed/nItG4ExlTaY'
-      },
-      {
-        name:'Hồng hạc',
-        link:'https://www.youtube.com/embed/0YM9Hz0q4Fk'
-      },
-      {
-        name:'Sóc',
-        link:'https://www.youtube.com/embed/HEj-L6wmngA'
-      },
-      {
-        name:'Nai',
-        link:'hhttps://www.youtube.com/embed/xAxay2axNR8'
-      },
-      {
-        name:'Rùa',
-        link:'https://www.youtube.com/embed/d4A0DwrVj6E'
-      },
-      {
-        name:'Chim cánh cụt',
-        link:'https://www.youtube.com/embed/rBqYxevAbWM'
-      },
-    ],
-    render: function() {
-      const htmls=this.videoList.map((video,index) => 
-        {
-          return `
+const swiperWrapperVideo = $(".video-animal .swiper-wrapper");
+const videoDisplay = $(".video-animal .video__category-place-display");
+const videos = {
+  currentIndex: 0,
+  videoList: [
+    {
+      name: "Khỉ",
+      link: "https://www.youtube.com/embed/LHpzjBydyGA",
+    },
+    {
+      name: "Ngựa vằn",
+      link: "https://www.youtube.com/embed/pI4KUIGrj9Y",
+    },
+    {
+      name: "Công",
+      link: "https://www.youtube.com/embed/fCx7VMnU6VY",
+    },
+    {
+      name: "Gấu",
+      link: "https://www.youtube.com/embed/q0BYoCNDK8w",
+    },
+    {
+      name: "Hươu cao cổ",
+      link: "https://www.youtube.com/embed/nItG4ExlTaY",
+    },
+    {
+      name: "Hồng hạc",
+      link: "https://www.youtube.com/embed/0YM9Hz0q4Fk",
+    },
+    {
+      name: "Sóc",
+      link: "https://www.youtube.com/embed/HEj-L6wmngA",
+    },
+    {
+      name: "Nai",
+      link: "hhttps://www.youtube.com/embed/xAxay2axNR8",
+    },
+    {
+      name: "Rùa",
+      link: "https://www.youtube.com/embed/d4A0DwrVj6E",
+    },
+    {
+      name: "Chim cánh cụt",
+      link: "https://www.youtube.com/embed/rBqYxevAbWM",
+    },
+  ],
+  render: function () {
+    const htmls = this.videoList.map((video, index) => {
+      return `
           <div class="swiper-slide">
-              <div class="video__category-option-item ${index===this.currentIndex ? 'active':''}" data-index=${index}>
+              <div class="video__category-option-item ${
+                index === this.currentIndex ? "active" : ""
+              }" data-index=${index}>
                   ${video.name}
               </div>
           </div>
-          `
-        })
-        swiperWrapperVideo.innerHTML=htmls.join('')
-    },
-    defineProperties: function(){
-      Object.defineProperty(this,'currentVideo',{
-        get:function()
-        {
-          return this.videoList[this.currentIndex]
-        }
-      })
-    },
-    handleEvent:function()
-    {
-        const _this = this;
-        swiperWrapperVideo.onclick = function(e)
-        {
-            var videoNodeActive=$('.video__category-option-item.active');
-            const videoNode=e.target.closest('.swiper-slide .video__category-option-item')
-            if(videoNode)
-            {
-              videoNodeActive.classList.remove('active');
-              _this.currentIndex=Number(videoNode.dataset.index);
-              _this.loadCurrentVideo();
-              videoNode.classList.add('active');
-              // _this.render();
-            }
-        }
-    },
-    loadCurrentVideo: function()
-    {
-        videoDisplay.src=this.currentVideo.link;
-    },
-    start:function()
-    {
-        // Lắng nghe và xử lý sự kiện
-        this.handleEvent();
-        // Định nghĩa thuộc tính cho Object
-        this.defineProperties();
-        // Tải thông tin video đầu tiên vào UI
-        this.loadCurrentVideo();
-        // Load ra danh sách video
-        this.render();
-    }
-}
-videos.start()
+          `;
+    });
+    swiperWrapperVideo.innerHTML = htmls.join("");
+  },
+  defineProperties: function () {
+    Object.defineProperty(this, "currentVideo", {
+      get: function () {
+        return this.videoList[this.currentIndex];
+      },
+    });
+  },
+  handleEvent: function () {
+    const _this = this;
+    swiperWrapperVideo.onclick = function (e) {
+      var videoNodeActive = $(".video__category-option-item.active");
+      const videoNode = e.target.closest(
+        ".swiper-slide .video__category-option-item"
+      );
+      if (videoNode) {
+        videoNodeActive.classList.remove("active");
+        _this.currentIndex = Number(videoNode.dataset.index);
+        _this.loadCurrentVideo();
+        videoNode.classList.add("active");
+        // _this.render();
+      }
+    };
+  },
+  loadCurrentVideo: function () {
+    videoDisplay.src = this.currentVideo.link;
+  },
+  start: function () {
+    // Lắng nghe và xử lý sự kiện
+    this.handleEvent();
+    // Định nghĩa thuộc tính cho Object
+    this.defineProperties();
+    // Tải thông tin video đầu tiên vào UI
+    this.loadCurrentVideo();
+    // Load ra danh sách video
+    this.render();
+  },
+};
+videos.start();
 
 // FORM ĐĂNG KÝ-ĐĂNG NHẬP
-const closeModalBtns=$$('.auth-form__icon-close');
-const modal=$('.modal')
-const authForm=$('.auth-form')
-const userForm=$('.user')
-const registerAuthForm=$('.auth-form__register')
-const loginAuthForm=$('.auth-form__login')
-const closeUserFormBtn=$('.user-close-btn')
-const openRegisterBtns=$$('.btn__open-register-form')
-const openLoginBtns=$$('.btn__open-login-form')
+const closeModalBtns = $$(".auth-form__icon-close");
+const modal = $(".modal");
+const authForm = $(".auth-form");
+const userForm = $(".user");
+const registerAuthForm = $(".auth-form__register");
+const loginAuthForm = $(".auth-form__login");
+const closeUserFormBtn = $(".user-close-btn");
+const openRegisterBtns = $$(".btn__open-register-form");
+const openLoginBtns = $$(".btn__open-login-form");
 
-const openUserInfo=$('.header__user-subnav-info')
+const openUserInfo = $(".header__user-subnav-info");
 
-for(const closeModalBtn of closeModalBtns) 
-{
-    closeModalBtn.onclick=function()
-    {
-        modal.classList.remove('open');
-        loginAuthForm.classList.remove('open');
-        registerAuthForm.classList.remove('open');
-    }
+function closeModal() {
+  modal.classList.remove("open");
+  loginAuthForm.classList.remove("open");
+  registerAuthForm.classList.remove("open");
+  userForm.classList.remove("open");
 }
-for(const openLoginBtn of openLoginBtns) 
-{
-    openLoginBtn.onclick=function()
-    {
-        modal.classList.add('open');
-        loginAuthForm.classList.add('open');
-        registerAuthForm.classList.remove('open');
-    }
-}
-for(const openRegisterBtn of openRegisterBtns) 
-{
-    openRegisterBtn.onclick=function()
-    {
-        modal.classList.add('open');
-        loginAuthForm.classList.remove('open');
-        registerAuthForm.classList.add('open');
-    }
-}
-openUserInfo.onclick=function()
-{
-    modal.classList.add('open');
-    userForm.classList.add('open');
-}
-function closeModal()
-{
-    modal.classList.remove('open');
-    loginAuthForm.classList.remove('open');
-    registerAuthForm.classList.remove('open');
-    userForm.classList.remove('open');
-}
-closeUserFormBtn.addEventListener('click',closeModal)
-modal.addEventListener('click',closeModal)
 
-// authForm.addEventListener('click',function(event)
-// {
-//       event.stopPropagation()
-// })
-userForm.addEventListener('click',function(event)
+for (const closeModalBtn of closeModalBtns) {
+  closeModalBtn.onclick = function () {
+      closeModal();
+  };
+}
+
+for (const openLoginBtn of openLoginBtns) {
+  openLoginBtn.onclick = function () {
+    modal.classList.add("open");
+    loginAuthForm.classList.add("open");
+    registerAuthForm.classList.remove("open");
+  };
+}
+for (const openRegisterBtn of openRegisterBtns) {
+  openRegisterBtn.onclick = function () 
+  {
+    modal.classList.add("open");
+    loginAuthForm.classList.remove("open");
+    registerAuthForm.classList.add("open");
+  };
+}
+
+closeUserFormBtn.addEventListener("click", closeModal);
+
+modal.addEventListener("click", closeModal);
+
+authForm.addEventListener('click',function(event)
 {
       event.stopPropagation()
 })
 
-
 // TAB USER FORM
-const userOptionItems=$$('.user__option-item');
-const userTabWrapItems=$$('.user__tab-wrap-item')
+openUserInfo.onclick = function () {
+  modal.classList.add("open");
+  userForm.classList.add("open");
+};
 
-userOptionItems.forEach(function(userOptionItem,index) 
-{
-  const userTabWrapItem=userTabWrapItems[index];
-  userOptionItem.onclick=function()
-  {
-    var userTabWrapItemActive=document.querySelector('.user__tab-wrap-item.active');
-    var userOptionItemActive=document.querySelector('.user__option-item.active');
+const userOptionItems = $$(".user__option-item");
+const userTabWrapItems = $$(".user__tab-wrap-item");
 
-    userTabWrapItemActive.classList.remove('active');
-    userOptionItemActive.classList.remove('active');
+userOptionItems.forEach(function (userOptionItem, index) {
+  const userTabWrapItem = userTabWrapItems[index];
+  userOptionItem.onclick = function () {
+    var userTabWrapItemActive = document.querySelector(
+      ".user__tab-wrap-item.active"
+    );
+    var userOptionItemActive = document.querySelector(
+      ".user__option-item.active"
+    );
 
-    userTabWrapItem.classList.add('active')
-    this.classList.add('active')
-  }
-})
+    userTabWrapItemActive.classList.remove("active");
+    userOptionItemActive.classList.remove("active");
+
+    userTabWrapItem.classList.add("active");
+    this.classList.add("active");
+  };
+});
+
+userForm.addEventListener("click", function (event) {
+  event.stopPropagation();
+});
