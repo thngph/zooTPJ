32621997ff1512a48661f903cbc876451256a342
thngph <19520981@gm.uit.edu.vn>
2021-11-09 20:53:09 +0700

    #

--- animals/models.py
+++ animals/models.py
@@ -7,6 +7,10 @@ from django.db import models
 class Animal(models.Model):
     animal_ID = models.AutoField(unique=True, primary_key=True)
     animal_name = models.CharField(max_length=50, blank=False)
-    description = models.CharField(max_length=1000)
+    description = models.CharField(max_length=1000, blank=True)
+    details = models.CharField(max_length=2000, blank=True)
     img = models.ImageField(default="", upload_to="photos/animals")
     profile_img = models.ImageField(default="", upload_to="photos/animals")
+
+    def __str__(self):
+        return self.animal_name
