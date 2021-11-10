c4694b117a485c0a3912a21294d59121fe3b5545
UIT19520097 <67829285+UIT19520097@users.noreply.github.com>
2021-11-10 16:36:22 +0700

    tách modal infoUser ra template riêng

--- home/urls.py
+++ home/urls.py
@@ -6,6 +6,7 @@ from .views import ProfileAPI, RegisterAPI
 urlpatterns = [
     path('', views.index),
     path('register/', views.register, name="register"),
+    path('infoUser/', views.infoUser, name="infoUser"),
     path('login/', auth_views.LoginView.as_view(template_name="home/login.html"), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('api/profile/', ProfileAPI.as_view(), name='profile_api_view'),
