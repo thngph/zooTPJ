76ef7b751daf1f58cd6a1fc6447c3e4b39152874
datdt <18520565@gm.uit.edu.vn>
2021-11-17 00:01:55 +0700

    hiển thị danh sách tin từ model ?có cần phân ra hot nhât vs gần nhất không? bấm vô thumbnail hiện ra bài post nhưng chưa load đc ảnh

--- event/urls.py
+++ event/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
-from .views import post_main
+from .models import Event
+# from .views import post_detail, news_list
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('post/', views.post_main, name='post'),
+    path('post/<int:event_id>', views.post_main, name='post'),
+    # path('', news_list.as_view(), name='news_list'),
+    # path('/post/<int:pk>', post_detail.as_view(), name='post_detail'),
 ]
--- event/views.py
+++ event/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from django.http import Http404
 from home.models import Profile
+from .models import Event
+from django.views.generic import DetailView
 
 
 # Create your views here.
@@ -9,10 +11,22 @@ from home.models import Profile
 def index(request):
     data = {}
     if request.user.is_authenticated:
-        data = {'Profile': Profile.objects.get(user_ID=request.user.id)}
+        data = {'Event': Event.objects.all(),
+                'Profile': Profile.objects.get(user_ID=request.user.id)}
+    else:
+        data = {'Event': Event.objects.all()}
     return render(request, 'event/news.html', data)
 
 
-def post_main(request):
-    data = {}
-    return render(request, 'event/post.html', data)
+def post_main(request, event_id):
+    try:
+        #     post = {}
+        #     if request.user.is_authenticated:
+        #         data = {'Event': Event.objects.get(condition="event_ID"),
+        #                 'Profile': Profile.objects.get(user_ID=request.user.id)}
+        #     else:
+        #         data = {'Event': Event.objects.all()}
+        post = Event.objects.get(event_ID=event_id)
+    except Event.DoesNotExit:
+        raise Http404("This Post does not exist")
+    return render(request, 'event/post.html', {'post': post})
