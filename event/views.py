154051f926035469d88f6e151a9d778dd1509cf5
datdt <18520565@gm.uit.edu.vn>
2021-11-23 18:00:24 +0700

    phan trang news chua co css

--- event/views.py
+++ event/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.http import Http404, HttpResponseRedirect
@@ -8,16 +9,47 @@ from django.views.generic import DetailView
 
 
 # Create your views here.
-
-
 def index(request):
+    event = Event.objects.all().order_by('-date_uploaded')
+    page_number = request.GET.get('page')
+    paginator = Paginator(event, 8)  # Show 8 posts mỗi page
+    try:
+        page = paginator.page(page_number)
+    except PageNotAnInteger:
+        # Nếu page_number không thuộc kiểu integer, trả về page đầu tiên
+        page = paginator.page(1)
+    except EmptyPage:
+        # Nếu page không có item nào, trả về page cuối cùng
+        page = paginator.page(paginator.num_pages)
     data = {}
     if request.user.is_authenticated:
-        data = {'Event': Event.objects.all().order_by('-date_uploaded'),
+        data = {'Event': page,
                 'Profile': Profile.objects.get(user_ID=request.user.id)}
     else:
-        data = {'Event': Event.objects.all()}
+        data = {'Event': page}
+    # Phantrang
     return render(request, 'event/news.html', data)
+
+# def index(request):
+#     event = Event.objects.all()
+#     page_number = int(request.GET.get('page', 1))
+#     paginator = Paginator(event, 8)  # Show 8 posts mỗi page
+#     try:
+#         page = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         # Nếu page_number không thuộc kiểu integer, trả về page đầu tiên
+#         page = paginator.page(1)
+#     except EmptyPage:
+#         # Nếu page không có item nào, trả về page cuối cùng
+#         page = paginator.page(paginator.num_pages)
+#     data = {}
+#     if request.user.is_authenticated:
+#         data = {'Event': Event.objects.all().order_by('-date_uploaded'),
+#                 'Profile': Profile.objects.get(user_ID=request.user.id)}
+#     else:
+#         data = {'Event': Event.objects.all()}
+#     # Phantrang
+#     return render(request, 'event/news.html', {'data': data})
 
 
 def post_main(request, event_id):
